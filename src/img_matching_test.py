dd4e46d42725e2e9837ce4f73410b63639938b01
we <david@wemakeprice.com>
2019-06-04 18:06:49 +0900

    add log euclidean metric

--- src/img_matching_test.py
+++ src/img_matching_test.py
@@ -31,10 +31,10 @@ target_img = cv2.imread(sample_path + sample_fiilename, 0)
 (w, h) = source_img.shape
 (t_w, t_h) = target_img.shape
 
-# print(w,h )
+print(w,h )
 # print(t_w, t_h)
 target_img = cv2.resize(target_img.copy(), (h,w), interpolation=cv2.INTER_CUBIC)
-# print(target_img.shape)
+print(target_img.shape)
 def log_euclidean_matrix(feature_mat):
     (U, s, V) = np.linalg.svd(feature_mat, full_matrices = True)
     print(U.shape)
@@ -61,52 +61,10 @@ diff_source_target = log_euc_source-log_euc_target
 
 avg_diff = abs(diff_source_target.sum()/diff_source_target.shape[0])
 
+print("avg_diff :", avg_diff)
 
-print(avg_diff)
-# print(log_svd)
-# print(template_temp-svd)
-# result_path = ""
-# result_name = ""
-#
-# akaze = cv2.AKAZE_create()
-#
-#
-# # Read chracter image and calculate feature quantity
-# expand_template = 2
-# whitespace = 20
-#
-# template_temp = cv2.imread(template_path + template_filename, 0)
-# height, width = template_temp.shape[:2]
-# template_img = np.ones((height + whitespace*2, width + whitespace*2), np.uint8) * 255
-#
-# template_img[whitespace:whitespace+ height, whitespace:whitespace+width] = template_temp
-# template_img = cv2.resize(template_img, None, fx = expand_template, fy = expand_template)
-# kp_temp, des_temp = akaze.detectAndCompute(template_img, None)
-#
-# # Load floor plan and calculate feature quantity
-# expand_sample = 2
-# sample_img = cv2.imread(sample_path+ sample_fiilename, 0)
-# sample_img = cv2.resize(sample_img, None, fx=expand_sample, fy=expand_sample)
-# kp_samp, des_samp = akaze.detectAndCompute(sample_img, None)
-#
-#
-# # Feature amount matching execution
-# bf = cv2.BFMatcher()
-# matches = bf.knnMatch(des_temp, des_samp, k=2)
-#
-# # Extract only those with high matching accuracy
-# ratio = 0.5
-# good = []
-# for m, n in matches:
-#     if m.distance < ratio*n.distance:
-#         good.append([m])
-#
-#
-# # Draw and save the matching result
-# cv2.namedWindow("Result", cv2.WINDOW_KEEPRATIO|cv2.WINDOW_NORMAL)
-# result_img = cv2.drawMatchesKnn(template_img, kp_temp, sample_img, kp_samp, good, None, flags=0)
-# cv2.imshow("Result", result_img)
-# cv2.waitKey(0)
+cv2.imshow("Result", target_img)
+cv2.waitKey(0)
 
 
 
